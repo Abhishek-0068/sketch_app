a68553db3de8ddb18418f7aeb3cea1889266d68d
Abhi3048 <learn8600.me@gmail.com>
2023-12-29 04:52:44 +0530

    toolBox slices written and ui is complete

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,7 +1,12 @@
 import { useEffect, useRef } from "react"
+import {useDispatch,useSelector} from 'react-redux'
 
 const Board = () => {
     const canvasRef = useRef(null)
+
+    const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+
+    const {color,size} = useSelector((state) => state.toolbox[activeMenuItem]);
 
     useEffect(() => {
         if(!canvasRef.current) return;
@@ -13,6 +18,7 @@ const Board = () => {
         canvas.height = window.innerHeight;
     },[])
 
+    // console.log(color,size)
     return(
         <canvas ref={canvasRef}></canvas>
     )
