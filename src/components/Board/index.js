34afc94ae09ef4f6240f4d97863c4b7c8ec97c5f
Abhi3048 <learn8600.me@gmail.com>
2023-12-29 07:59:42 +0530

    undo-redo functionality added

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -7,6 +7,8 @@ const Board = () => {
     const dispatch = useDispatch();
     const canvasRef = useRef(null)
     const shouldDraw = useRef(false)
+    const drawHistory = useRef([])
+    const historyPointer = useRef(0);
 
     const {activeMenuItem, actionMenuItem} = useSelector((state) => state.menu);
 
@@ -24,6 +26,13 @@ const Board = () => {
             anchor.download = 'sketch.png'
             anchor.click()
             console.log(URL)
+        }else if( actionMenuItem === MENU_ITEMS.UNDO || actionMenuItem === MENU_ITEMS.REDO){
+            if( historyPointer.current > 0 && actionMenuItem === MENU_ITEMS.UNDO ) historyPointer.current -= 1;
+            if( historyPointer.current < drawHistory.current.length - 1 && actionMenuItem === MENU_ITEMS.REDO ) historyPointer.current += 1;
+            
+            const imageData = drawHistory.current[historyPointer.current]
+            context.putImageData(imageData,0,0)
+            
         }
         dispatch(actionItemClick(null))
     },[actionMenuItem,dispatch])
@@ -71,6 +80,9 @@ const Board = () => {
 
         const handleMouseUp = (e) => {
             shouldDraw.current = false
+            const imageData = context.getImageData(0,0,canvas.width,canvas.height);
+            drawHistory.current.push(imageData)
+            historyPointer.current = drawHistory.current.length - 1
         }
 
         canvas.addEventListener('mousedown',handleMouseDown)
